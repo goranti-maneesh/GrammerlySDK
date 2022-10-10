16140d935293cabccf71015f2116e8835039b806
goranti-maneesh <gorantimaneeshreddy@gmail.com>
2022-10-10 22:14:18 +0530

    integrating react quill

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-// import logo from './logo.svg';
-import { GrammarlyEditorPlugin } from "@grammarly/editor-sdk-react";
+import { Component } from "react";
+import { GrammarlyEditorPlugin  } from "@grammarly/editor-sdk-react";
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css';
+
 import './App.css';
 
-function App() {
-  return (
-    <GrammarlyEditorPlugin clientId="client_DUccJyyPF61B699b6kMiTt">
-      <div className="App">
-        <textarea rows={20} cols={50}></textarea>
-      </div>
-    </GrammarlyEditorPlugin>
-  );
+const modules = { // modules are used to customize the text bar.
+  toolbar: [
+    [{ 'header': [1, 2, false] }],
+    ['bold', 'italic', 'underline','strike', 'blockquote'],
+    [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+    ['link', 'image'],
+    ['clean']
+  ],
+}
+
+class App extends Component {
+  state={text:""}
+
+  onChangeText = (event) => {
+    this.setState({text: event.target.value})
+  }
+
+  render(){
+    const {text, onChangeText} = this.state
+    return (
+      <GrammarlyEditorPlugin clientId="client_DUccJyyPF61B699b6kMiTt">
+          <ReactQuill value={text} onChange={onChangeText} modules={modules}/>
+      </GrammarlyEditorPlugin>
+    );
+  }
 }
 
 export default App;
